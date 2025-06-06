72a22f346b34977563aafe77d828188e2f349d47
VladosIsFunnyGuy <vladyslav.rohovyi@nure.ua>
2025-06-06 15:57:53 +0300

    try vercel

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php
 
-echo "Hello world!";
+$uri = trim($_SERVER['REQUEST_URI'], '/');
 
-require __DIR__ . '/modules/home-page/controllers/home.php';
-
+switch ($uri)
+{
+    case '':
+    case 'home':
+        require __DIR__ . '/modules/home-page/controllers/home.php';
+        break;
+    case 'about':
+        require __DIR__ . '/modules/about-us/controllers/about-us.php';
+        break;
+    // Add more routes here...
+    default:
+        http_response_code(404);
+        echo "Page not found.";
+}
 ?>
